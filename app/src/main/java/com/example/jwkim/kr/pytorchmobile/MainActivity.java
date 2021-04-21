7715910999bbf8c6035e738e1938fdac9f04c9fb
jaewoong.kim.29@gmail.com <jaewoong.kim.29@gmail.com>
2021-04-22 00:24:17 +0900

    Added Log: API version

--- app/src/main/java/com/example/jwkim/kr/pytorchmobile/MainActivity.java
+++ app/src/main/java/com/example/jwkim/kr/pytorchmobile/MainActivity.java
@@ -5,7 +5,9 @@ package com.example.jwkim.kr.pytorchmobile;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -38,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_test.setOnClickListener(this);
         btn_gallery.setOnClickListener(this);
         btn_exit.setOnClickListener(this);
+
+        Log.i(getString(R.string.tag), "API version is " + Build.VERSION.SDK_INT);
     }
 
     @Override
